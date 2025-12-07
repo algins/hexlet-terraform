0f40d2901e52ef27939a01f7506cf5b5211f65f1
algins <igors.algins@gmail.com>
2025-12-07 11:34:08 +0200

    feat: import existing resources

--- main.tf
+++ main.tf
@@ -13,8 +13,15 @@ provider "digitalocean" {
   token = var.do_token
 }
 
-resource "digitalocean_droplet" "example" {
-  name = "ubuntu-s-1vcpu-512mb-10gb-ams3-01"
+resource "digitalocean_droplet" "web1" {
+  name = "web1"
+  region = "ams3"
+  size = "s-1vcpu-1gb"
+  image = "ubuntu-24-04-x64"
+}
+
+resource "digitalocean_droplet" "web2" {
+  name = "web2"
   region = "ams3"
   size = "s-1vcpu-1gb"
   image = "ubuntu-24-04-x64"
