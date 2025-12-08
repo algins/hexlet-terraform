8696ec857ca0759b1893b3f3e3c245a8ff22c724
algins <igors.algins@gmail.com>
2025-12-08 19:06:09 +0200

    feat: add loadbalancer

--- main.tf
+++ main.tf
@@ -13,6 +13,44 @@ provider "digitalocean" {
   token = var.do_token
 }
 
+resource "digitalocean_loadbalancer" "lb" {
+  name = "lb"
+  region = "ams3"
+
+  redirect_http_to_https = true
+  enable_backend_keepalive = true
+
+  forwarding_rule {
+    entry_protocol = "http"
+    entry_port = 80
+    target_protocol = "http"
+    target_port = 80
+  }
+
+  forwarding_rule {
+    entry_protocol = "https"
+    entry_port = 443
+    target_protocol = "http"
+    target_port = 80
+    certificate_name = "source.id.lv"
+  }
+
+  healthcheck {
+    protocol = "http"
+    port = 80
+    path = "/login"
+    check_interval_seconds = 10
+    response_timeout_seconds = 5
+    healthy_threshold = 5
+    unhealthy_threshold = 3
+  }
+
+  droplet_ids = [
+    digitalocean_droplet.web1.id,
+    digitalocean_droplet.web2.id
+  ]
+}
+
 resource "digitalocean_droplet" "web1" {
   name = "web1"
   region = "ams3"
